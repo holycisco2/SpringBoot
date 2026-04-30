1ab616426914838bd59344ef0524791add80bfcf
holycisco2 <holycisco2@gmail.com>
2026-04-30 17:19:45 +0800

    Delete legacy files

--- Springboot-todolist/src/main/java/com/example/todolist/exception/TodoNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.todolist.exception;
-
-// 找不到代辦事項資料
-public class TodoNotFoundException extends Exception {
-	public TodoNotFoundException(String message) {
-		super(message);
-	}
-}
